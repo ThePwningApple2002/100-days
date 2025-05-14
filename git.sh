b52de6f1bbd1040496665ec3b8740d4e7dc41159
thepwningapple2002 <milan.265.kotd@gmail.com>
2025-05-14 15:46:21 +0200

    Auto-commit: 2025-05-14 15:46:21

--- git.sh
+++ git.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+RED='\033[0;31m'
 # Get current date and time for the commit message
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
@@ -12,4 +12,4 @@ git commit -m "Auto-commit: $TIMESTAMP"
 # Push to the current branch
 git push
 
-echo "Changes committed and pushed with timestamp: $TIMESTAMP"
+echo -e  "${RED}Changes committed and pushed with timestamp: $TIMESTAMP"
